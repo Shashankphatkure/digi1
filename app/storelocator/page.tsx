ff3df086ad2cc0777b3d05facc45022a582e1e81
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-11 04:49:23 +0530

    added required components

---
+++ app/storelocator/page.tsx
@@ -0,0 +1,9 @@
+import Gridlists from "@/components/applicationui/lists/gridlists/contact_cards_with_small_portraits";
+
+export default function Home() {
+  return (
+    <>
+    <Gridlists/>
+    </>
+  );
+}
