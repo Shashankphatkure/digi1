c2e62fe2abaf343c929f749d26f3703be07f912d
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-11 05:17:54 +0530

    added 404 and created pages

---
+++ app/aboutus/page.tsx
@@ -0,0 +1,9 @@
+import Aboutuspage from "@/components/marketing/aboutuspage/with_image_tiles";
+
+export default function Home() {
+  return (
+    <>
+    <Aboutuspage/>
+    </>
+  );
+}
--- components/marketing/aboutuspage/with_image_tiles.tsx
+++ components/marketing/aboutuspage/with_image_tiles.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Footer from '../section/footer/4_column_with_newsletter_dark'
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -545,28 +546,8 @@ export default function Aboutuspage() {
       </main>
 
       {/* Footer */}
-      <footer className="mx-auto mt-40 max-w-7xl overflow-hidden px-6 pb-20 sm:mt-64 sm:pb-24 lg:px-8">
-        <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
-          {footerNavigation.main.map((item) => (
-            <div key={item.name} className="pb-6">
-              <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-                {item.name}
-              </a>
-            </div>
-          ))}
-        </nav>
-        <div className="mt-10 flex justify-center space-x-10">
-          {footerNavigation.social.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
-              <span className="sr-only">{item.name}</span>
-              <item.icon className="h-6 w-6" aria-hidden="true" />
-            </a>
-          ))}
-        </div>
-        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          &copy; 2020 Your Company, Inc. All rights reserved.
-        </p>
-      </footer>
+      <Footer/>
+
     </div>
   )
 }
