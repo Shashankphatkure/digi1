00652e8da81d5131cc46540d3038371e65353451
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-11 19:48:13 +0530

    updated account settings and store locator

--- app/accountsettings/page.tsx
+++ app/accountsettings/page.tsx
@@ -1,9 +1,15 @@
 import Profilesettings from "@/components/applicationui/forms/formlayouts/two_column_with_cards";
+import Ecommerceheader from "@/components/marketing/elements/header/with_left_aligned_nav";
+import Footer from "@/components/marketing/section/footer/4_column_with_newsletter_dark";
 
 export default function Home() {
   return (
     <>
-    <Profilesettings/>
+    <Ecommerceheader/>
+    <div style={{ padding: '10px', margin: '10px' }}>
+  <Profilesettings/>
+</div>
+    <Footer/>
     </>
   );
 }
--- app/storelocator/page.tsx
+++ app/storelocator/page.tsx
@@ -1,9 +1,17 @@
 import Gridlists from "@/components/applicationui/lists/gridlists/contact_cards_with_small_portraits";
+import Ecommerceheader from "@/components/marketing/elements/header/with_left_aligned_nav";
+import Footer from "@/components/marketing/section/footer/4_column_with_newsletter_dark";
 
 export default function Home() {
   return (
     <>
+    <Ecommerceheader/>
+    
+    <div style={{ padding: '10px', margin: '10px' }}>
     <Gridlists/>
+    </div>
+    
+    <Footer/>
     </>
   );
 }
