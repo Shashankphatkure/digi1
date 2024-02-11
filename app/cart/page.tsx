a49b8055b075144e5e9a1be837fc0d02e18fa2d4
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-11 18:30:35 +0530

    updated header and footers

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,4 +1,5 @@
 import Cartpage from "@/components/ecommerce/cartpage/with_related_products";
+import Header from "@/components/marketing/elements/header/with_left_aligned_nav";
 
 export default function Home() {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Reviews from "@/components/ecommerce/reviews/with_summary_chart";
 import Banners from "@/components/marketing/elements/banners/with_button";
+import Header from "@/components/marketing/elements/header/with_left_aligned_nav";
 import Blogsection from "@/components/marketing/section/blogsection/three_column_with_images";
 import Contentsection from "@/components/marketing/section/contentsection/with_testimonial_and_stats";
 import Footer from "@/components/marketing/section/footer/4_column_with_newsletter_dark";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
     <Banners />
+    <Header />
     <Contentsection />
     <Blogsection />
     <Reviews/>
