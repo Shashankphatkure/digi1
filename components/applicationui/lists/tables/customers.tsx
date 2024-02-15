0934fd331bee2812acc394ce745b1396213d1193
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-15 15:19:46 +0530

    updated with supabase data for products and customers

--- components/applicationui/lists/tables/customers.tsx
+++ components/applicationui/lists/tables/customers.tsx
@@ -1,49 +1,16 @@
-const people = [
-  {
-    name: 'Lindsay Walton',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    phone: '8433804507',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Lindsay Walton',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    phone: '8433804507',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Lindsay Walton',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    phone: '8433804507',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Lindsay Walton',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    phone: '8433804507',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  
-  // More people...
-]
+'use client'
 
-export default function Customerstable() {
+async function getData() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/customer?apikey=${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`, { cache: 'no-store' })
+  return res.json()
+}
+
+export default async function Customerstable() {
+
+  const data = await getData()
+
+  console.log(data)
+
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
@@ -72,16 +39,16 @@ export default function Customerstable() {
                     Full Name
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    Total Orders
+                    Phone & Email
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                     Status
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    Last purchase
+                    Orders
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    Phone number
+                    Last Order
                   </th>
                   <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
                     <span className="sr-only">Edit</span>
@@ -89,33 +56,33 @@ export default function Customerstable() {
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200 bg-white">
-                {people.map((person) => (
-                  <tr key={person.email}>
+                {data.map((data: any) => (
+                  <tr key={data.name}>
                     <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
                       <div className="flex items-center">
                         <div className="h-11 w-11 flex-shrink-0">
-                          <img className="h-11 w-11 rounded-full" src={person.image} alt="" />
+                          <img className="h-11 w-11 rounded-full" src={data.profilephoto} alt="" />
                         </div>
                         <div className="ml-4">
-                          <div className="font-medium text-gray-900">{person.name}</div>
-                          <div className="mt-1 text-gray-500">{person.email}</div>
+                          <div className="font-medium text-gray-900">{data.name}</div>
+                          <div className="mt-1 text-gray-500">{data.price}</div>
                         </div>
                       </div>
                     </td>
                     <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <div className="text-gray-900">{person.title}</div>
-                      <div className="mt-1 text-gray-500">Last purchase: {person.department}</div>
+                      <div className="text-gray-900">{data.email}</div>
+                      <div className="text-gray-900">{data.phone}</div>
                     </td>
                     <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
                       <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                         Active
                       </span>
                     </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{person.role}</td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{person.phone}</td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{data.totalorders}</td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{data.lastorder}</td>
                     <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                       <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                        Edit<span className="sr-only">, {person.name}</span>
+                        Edit<span className="sr-only">, {data.name}</span>
                       </a>
                     </td>
                   </tr>
--- components/applicationui/lists/tables/products.tsx
+++ components/applicationui/lists/tables/products.tsx
@@ -1,49 +1,14 @@
-const people = [
-  {
-    name: 'Lindsay Walton',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    phone: '8433804507',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Lindsay Walton',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    phone: '8433804507',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Lindsay Walton',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    phone: '8433804507',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Lindsay Walton',
-    title: 'Front-end Developer',
-    department: 'Optimization',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
-    phone: '8433804507',
-    image:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  
-  // More people...
-]
+'use client'
 
-export default function Productstable() {
+async function getData() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/product?apikey=${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`, { cache: 'no-store' })
+  return res.json()
+}
+
+export default async function Productstable() {
+
+  const data = await getData()
+
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
@@ -69,19 +34,19 @@ export default function Productstable() {
               <thead>
                 <tr>
                   <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
-                    Full Name
+                    Product Name
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    Total Orders
+                    Category
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                     Status
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    Last purchase
+                    Quantity
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    Phone number
+                    Brand
                   </th>
                   <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
                     <span className="sr-only">Edit</span>
@@ -89,33 +54,32 @@ export default function Productstable() {
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200 bg-white">
-                {people.map((person) => (
-                  <tr key={person.email}>
+                {data.map((data: any) => (
+                  <tr key={data.name}>
                     <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
                       <div className="flex items-center">
                         <div className="h-11 w-11 flex-shrink-0">
-                          <img className="h-11 w-11 rounded-full" src={person.image} alt="" />
+                          <img className="h-11 w-11 rounded-full" src={data.featured_image} alt="" />
                         </div>
                         <div className="ml-4">
-                          <div className="font-medium text-gray-900">{person.name}</div>
-                          <div className="mt-1 text-gray-500">{person.email}</div>
+                          <div className="font-medium text-gray-900">{data.name}</div>
+                          <div className="mt-1 text-gray-500">{data.price}</div>
                         </div>
                       </div>
                     </td>
                     <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <div className="text-gray-900">{person.title}</div>
-                      <div className="mt-1 text-gray-500">Last purchase: {person.department}</div>
+                      <div className="text-gray-900">{data.category}</div>
                     </td>
                     <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
                       <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                         Active
                       </span>
                     </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{person.role}</td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{person.phone}</td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{data.quantity}</td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{data.brand}</td>
                     <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                       <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                        Edit<span className="sr-only">, {person.name}</span>
+                        Edit<span className="sr-only">, {data.name}</span>
                       </a>
                     </td>
                   </tr>
